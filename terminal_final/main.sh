1e96e40000c6cee3af349ffa596cf6430013fd7d
vapemo <vale_perez@ciencias.unam.mx>
2025-04-25 00:46:03 -0600

    ajuste main

--- terminal_final/main.sh
+++ terminal_final/main.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# bloqueo de  Ctrl+C y Ctrl+Z
+# bloqueo de Ctrl+C y Ctrl+Z
 trap '' SIGINT
 trap '' SIGTSTP
 
-# personalización
+# colores
 GREEN='\033[0;32m'
 BLUE='\033[1;34m'
-NC='\033[0m' 
+NC='\033[0m'
 
 while true; do
   USER=$(whoami)
@@ -15,6 +15,7 @@ while true; do
   echo -e "\n${GREEN}${USER}${NC}@${BLUE}${CURRENT_DIR}${NC} ➜ Escribe un comando ('ayuda' para ver lista)"
   echo -n "➜ "
   read input
+  input=${input,,}  
 
   case $input in
     ayuda) bash ayuda.sh ;;
@@ -22,10 +23,16 @@ while true; do
     fecha) bash fecha.sh ;;
     buscar*) bash buscar.sh $input ;;
     creditos) bash creditos.sh ;;
-    juego) bash juego.sh ;;
-    mp3) bash mp3.sh ;;
+    juego) bash ./ahorcado.sh ;;
+    mp3) bash ./mp3.sh ;;
     salir) bash salir.sh && exit 0 ;;
-    *) echo " Comando incorrecto: $input" ;; #listado (ayuda,infosis,fecha,buscar,creditos(nuestros nombres),juego ,reproductor,salida )
+    *)
+      if command -v "$input" >/dev/null 2>&1; then
+        eval "$input"
+      else
+        echo "Error: $input"
+      fi
+      ;;
   esac
 done
 
