ef82ae62bafec784369049cbd7773f9baeb3167e
vapemo <vale_perez@ciencias.unam.mx>
2025-04-25 00:33:48 -0600

    último ajuste

--- terminal_final/ayuda.sh
+++ terminal_final/ayuda.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
+
+# colores
+MAGENTA='\033[1;35m'
+AZUL='\033[1;34m'
+VERDE='\033[1;32m'
+NC='\033[0m' 
+
 printf "\n\n"
-echo "-----A Y U D A-----"
+echo -e "${MAGENTA}-----A Y U D A-----${NC}"
 printf "\n\n"
-echo "Comando           Descripcion"
-echo  "-------------------------------------------------------------------"
-echo "infosis           Informacion del sistema que muestra: Memoria RAM - Arquitectura del sistema - Version del SO"
-echo "---------------------------------------------------------------------"
-echo "fecha             Muestra la fecha y hora actual"
-echo "--------------------------------------------------------------------"
-echo "buscar            Buscar un archivo en un directorio en especifico (recibe dos parametros-La carpeta y el archivo a buscar)"
-echo "--------------------------------------------------------------------"
-echo "creditos          Muestra los creditos de los que programaron"
-echo "--------------------------------------------------------------------"
-echo "juego             Juego"
-echo "------------------------------------------------------------------"
-echo "mp3            Escuchar musica"
-echo "------------------------------------------------------------------"
-echo "salir             Salir de la terminal"
+echo -e "${VERDE}Comando           ${AZUL}Descripción${NC}"
+echo -e "${MAGENTA}-------------------------------------------------------------------${NC}"
+echo -e "${VERDE}infosis${NC}           ${AZUL}Información del sistema que muestra: Memoria RAM - Arquitectura del sistema - Version del SO${NC}"
+echo -e "${MAGENTA}---------------------------------------------------------------------${NC}"
+echo -e "${VERDE}fecha${NC}             ${AZUL}Muestra la fecha y hora actual${NC}"
+echo -e "${MAGENTA}--------------------------------------------------------------------${NC}"
+echo -e "${VERDE}buscar${NC}            ${AZUL}Buscar un archivo en un directorio en especifico (recibe dos parametros-La carpeta y el archivo a buscar)${NC}"
+echo -e "${MAGENTA}--------------------------------------------------------------------${NC}"
+echo -e "${VERDE}creditos${NC}          ${AZUL}Muestra los creditos de los que programaron${NC}"
+echo -e "${MAGENTA}--------------------------------------------------------------------${NC}"
+echo -e "${VERDE}juego${NC}             ${AZUL}Juego${NC}"
+echo -e "${MAGENTA}------------------------------------------------------------------${NC}"
+echo -e "${VERDE}mp3${NC}               ${AZUL}Escuchar música${NC}"
+echo -e "${MAGENTA}------------------------------------------------------------------${NC}"
+echo -e "${VERDE}salir${NC}             ${AZUL}Salir de la terminal${NC}"
 printf "\n\n"
+
