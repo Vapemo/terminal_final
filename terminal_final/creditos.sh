38be369fb6438587528f8cba77bb8b0e3bd30e04
CamilaAlexandraSalas <112556680+CamilaAlexandraSalas@users.noreply.github.com>
2025-04-26 14:14:55 -0600

    correccion final creditos

--- terminal_final/creditos.sh
+++ terminal_final/creditos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo -e "\n\e[33m╔══════════════════════════════╗"
+echo -e "║        🎬  CRÉDITOS  🎬         ║"
+echo -e "╚══════════════════════════════╝\e[0m\n"
+
 echo -e "\n\t${am}👨‍💻 Desarrollado por:"
 echo -e "\t${v}   • Camila Salas"
 echo -e "\t${v}   • Valeria Perez"
